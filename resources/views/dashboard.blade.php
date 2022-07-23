f2f815c13fc8e66c59aeb3215293161e6363c74c
David <workingnown@gmail.com>
2022-07-23 12:53:43 -0300

    Instalação do Laravel e Configuração do Banco

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,36 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Faça aqui a escolha do seu Curso!') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white card shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                   Designer Gráfico
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white card shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                   Análise e Desenvolvimento de Sistemas
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-6" >
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white card shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200 ">
+                    Arquitetura
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
